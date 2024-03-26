b2d15c4ead39bf8fecf527be6fd84c0c4713fcb1
'Mabin' <'mabinum9528@naver.com'>
2024-03-26 12:45:20 +0900

    프로젝트3

---
+++ src/Quiz/Quiz1.java
@@ -0,0 +1,17 @@
+package Quiz;
+
+public class Quiz1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = -10; // 10
+		int result = +num; // -10
+		
+		int num2 = -20; // -20 
+		int result2 = -num2; //20
+		
+		System.out.println(result);
+		System.out.println(result2);
+	}
+
+}
---
+++ src/Quiz/Quiz2.java
@@ -0,0 +1,19 @@
+package Quiz;
+
+public class Quiz2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 10; //11
+		
+		int y = 20; //19
+		
+		int result = (x++) + (--y); //29
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(result);
+	}
+
+}
